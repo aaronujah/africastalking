927566b785847bb017cde2290521c653e598a8e6
Aaron Ujah <aaronujah@gmail.com>
2022-09-29 17:26:40 +0100

    Add router

--- route.js
+++ route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const ussdController = require("./ussd");
 
-router.route("/ussd").post(ussdController.ussd);
+router.route("/").post(ussdController.ussd);
 
 module.exports = router;
