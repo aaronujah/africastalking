dff2ecb113550aafdb0e56ad401ad02e2e30c5fb
Aaron Ujah <aaronujah@gmail.com>
2022-09-29 21:07:44 +0100

    Fix errors

--- route.js
+++ route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ussdController = require("./ussd");
 
 router.route("/").post(ussdController.ussd);
+// .get(ussdController.getMe);
 
 module.exports = router;
